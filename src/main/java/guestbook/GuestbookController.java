a28d148e4c2bca7a8ef075d312de809f8615fafd
Chiaki Watanabe <Chiaki.Watanabe@stud.hs-merseburg.de>
2023-04-03 23:29:10 +0200

    Controller-Methode und html-code fuer update hinzugefuegt, fehlerhaft

--- src/main/java/guestbook/GuestbookController.java
+++ src/main/java/guestbook/GuestbookController.java
@@ -26,13 +26,17 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 /**
  * A controller to handle web requests to manage {@link GuestbookEntry}s
@@ -123,9 +127,32 @@ class GuestbookController {
 		return entry.map(it -> {
 
 			guestbook.delete(it);
+		
 			return "redirect:/guestbook";
 
 		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}
+	
+
+	
+	/*
+	 * Methode für Editieren
+	 * */
+	@PreAuthorize("hasRole('ADMIN')")
+	@PostMapping(path="/guestbook/{entry}")
+	String updateEntry(@Valid @ModelAttribute("form") GuestbookForm form, @PathVariable Optional<GuestbookEntry> entry){
+		
+		return entry.map(it -> {
+			
+			it.setName(form.getName());
+			it.setText(form.getText());
+			it.setEmail(form.getEmail());
+			guestbook.save(it);
+		
+			return "redirect:/guestbook";
+
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+		
 	}
 
 	// Request methods answering HTMX requests
@@ -142,6 +169,8 @@ class GuestbookController {
 	 * @return a reference to a Thymeleaf template fragment
 	 * @see #addEntry(String, String)
 	 */
+	
+	
 	@HxRequest
 	@PostMapping(path = "/guestbook")
 	HtmxResponse addEntry(@Valid GuestbookForm form, Model model) {
@@ -178,4 +207,24 @@ class GuestbookController {
 
 		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 	}
+	
+	/*
+	@HxRequest
+	@PreAuthorize("hasRole('ADMIN')")
+	@PutMapping(path = "/guestbook/{entry}")
+	HtmxResponse updateEntryHtmx(@Valid GuestbookForm form, @PathVariable Optional<GuestbookEntry> entry, Model model) {
+		
+		return entry.map(it -> {
+			it.setName(form.getName());
+			it.setText(form.getText());
+			it.setEmail(form.getEmail());
+			guestbook.save(it);
+
+			model.addAttribute("entries", guestbook.findAll());
+
+			return new HtmxResponse()
+					.addTemplate("guestbook :: entries");
+
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+	}*/
 }
